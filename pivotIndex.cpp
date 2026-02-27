55b74d6b8ffb61ac301394f8f2574fe9a7f31571
Kiruthika <164878094+kiru1711@users.noreply.github.com>
2026-02-27 11:45:55 +0530

    left sum == right sum

---
+++ pivotIndex.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+        }int leftsum = 0;
+        for(int i=0;i<nums.size();i++){
+            int rightsum = sum - leftsum - nums[i];
+            if(leftsum == rightsum) return i;
+            leftsum += nums[i];
+        }return -1;
+    }
+};
